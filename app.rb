d481ce12b799b446f23debf85b158f8d57cf30bc
Josh Delsman <jdelsman@voxxit.com>
2012-08-02 09:50:18 -0700

    Adding models

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 DataMapper::Logger.new(STDOUT, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "mysql://localhost/app_development")
 
+Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |model| require model }
+
 class App < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
